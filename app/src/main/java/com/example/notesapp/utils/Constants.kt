b386bc2f619fb190f2b6d0fa7d36245e250fb9ba
Prince Kumar <kumarprince99990@gmail.com>
2023-01-04 00:07:11 +0530

    first commit

---
+++ app/src/main/java/com/example/notesapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.notesapp.utils
+
+object Constants {
+    const val TAG = "MYCHEEZYCODE"
+    const val  BASE_URL ="https://cute-pear-rattlesnake.cyclic.app/"
+    const val PREFS_TOKEN_FILE = "PREFS_TOKEN_FILE"
+    const val USER_TOKEN = "USER_TOKEN"
+}
